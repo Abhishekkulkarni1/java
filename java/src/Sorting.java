2ae6edd8a78040f4916b18ac5ea164306fd3844b
Abhishekkulkarni1 <11432abhi@gmail.com>
2025-04-19 13:17:48 +0530

    sorting algo

---
+++ java/src/Sorting.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class Sorting {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 6, 8, 15, 12, 2};
+        System.out.println(Arrays.toString(sortArray(arr)));
+    }
+    public static int[] sortArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+
+    public static void bubbleSort(int[] arr, int n) {
+        for (int i = n - 1; i >= 0; i--) {
+            int didSwap = 0;
+
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    didSwap = 1;
+                }
+            }
+            if (didSwap == 0) {
+                break;
+            }
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                // Swap arr[j] and arr[j-1]
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+}
